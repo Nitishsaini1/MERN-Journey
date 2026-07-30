29a42fc514ed5df75b02da973e98613a5abd90e3
Nitish Kumar <ns7410995@gmail.com>
2026-07-30 13:20:08 +0530

    Complete weather app functionality with API integration

--- Day27-Weather_app/algorithm.md
+++ Day27-Weather_app/algorithm.md
@@ -31,3 +31,82 @@ so that we can show our data on the screen
 Display weather  
       ↓
  
+
+ ******************************
+
+fetch receive the city input and run the url 
+       ↓
+it return promise
+↓
+if that promise is fulfill it find .then() and if that promise is rejected it find catch and show error
+↓
+now when the promise is fullfilled it go to .then() and then that that then receive the response object by the response
+↓
+and that response object will have the response body inside it and now comes main part when we receive that response it will be in a json text when we cannot acces or read so for that we need to turn that json text to object for that we use .json() so that we can access our response body 
+↓
+and then it will also trigger the another promise for which we need the .then()
+↓
+and here that .then(data) will point to the response body through which we can display our content which we like 
+↓
+and by also making a displayWeather function outside and calling it inside the .then(data) we can access a block scope function outside like this 
+displayWeather(data) by this way we can show what we want on screen 
+
+
+
+==> Funciton are not block scoped in the way variable declared with `let` are
+
+``
+data is local to the .then(..) callback
+you pass that data as an argument to displayweather
+inside displayweather, that argument becomes its parameter
+``
+
+
+
+```
+{
+  "coord": {
+    "lon": 77.2167,
+    "lat": 28.6667
+  },
+  "weather": [
+    {
+      "id": 804,
+      "main": "Clouds",
+      "description": "overcast clouds",
+      "icon": "04d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 37,
+    "feels_like": 41.62,
+    "temp_min": 37,
+    "temp_max": 37,
+    "pressure": 998,
+    "humidity": 41,
+    "sea_level": 998,
+    "grnd_level": 974
+  },
+  "visibility": 10000,
+  "wind": {
+    "speed": 4.21,
+    "deg": 120,
+    "gust": 4.03
+  },
+  "clouds": {
+    "all": 91
+  },
+  "dt": 1785394055,
+  "sys": {
+    "country": "IN",
+    "sunrise": 1785370263,
+    "sunset": 1785419035
+  },
+  "timezone": 19800,
+  "id": 1273294,
+  "name": "Delhi",
+  "cod": 200
+}
+
+```
--- Day27-Weather_app/script.js
+++ Day27-Weather_app/script.js
@@ -3,35 +3,60 @@ const submitBtn = document.querySelector(".submit-btn");
 const warning = document.querySelector(".warning")
 const cityName = document.querySelector(".city-name");
 const temprature = document.querySelector(".temprature");
-const weather = document.querySelector(".weather");
+const weatherCheck = document.querySelector(".weather");
 const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed")
+console.log("run");
 
 function cityNameInput (){ 
-    let userInput = cityInput.value;
+    let userInput = cityInput.value.trim();
 
-    if (userInput.trim()=="") {
+    if (userInput ==="") {
         warning.innerText = "Please enter the name"
-    } else {
+    } else { 
         for (let i = 0; i < userInput.length; i++) {
            let  currentCharacter = userInput[i]; 
-           console.log(currentCharacter);
            if (currentCharacter >= "0" && currentCharacter <= "9") { 
-                warning.innerText = "City Name is invalid";
-                console.log(currentCharacter);
-                
+                warning.innerText = "City Name is invalid"; 
                 return;
-            } else {
-                
-            }
-            
-            
+            }  
         }
+         let url = 'https://api.openweathermap.org/data/2.5/weather?q='+userInput+'&appid=3e02a9ecde5bba9c92576cd374488488&units=metric';
+         fetch(url)
+         .then(function (response){
+            return response.json();
+         }
+        )
+         .then(
+            function (data) {
+                displayWeather(data);
+            }
+         )
+         .catch(
+         function (error) {
+            console.log(error);
+            
+             warning.innerText = "The Weather cannot load right now !"
+            
+         }
+         
+
+         )
+
          cityInput.value="";
     warning.innerText = "";
     }
    
 }
 
+function displayWeather(weather){
+    cityName.innerText =  weather.name;
+    temprature.innerText = weather.main.temp;
+    humidity.innerText = weather.main.humidity;
+    windSpeed.innerText = weather.wind.speed;
+    weatherCheck.innerText = weather.weather[0].description;
+}
+ 
+submitBtn.addEventListener("click", cityNameInput)
 
 
-submitBtn.addEventListener("click", cityNameInput) 
