7db50bfee3199befa5d5ead675645016de91709b
Nitish Kumar <ns7410995@gmail.com>
2026-08-18 20:33:18 +0530

    build counter with controlled step input

--- React/React Learning/src/Counter.jsx
+++ React/React Learning/src/Counter.jsx
@@ -1,34 +1,39 @@
 import {useState} from 'react'
 
 function Counter(){
-    const [counter, setCounter] = useState(0)
-    const [step, setStep] = useState(0)
+    const [counter, setCounter] = useState(0);
+    const [step, setStep] = useState(5)
+    const [stepInput, setStepInput] = useState("5")
+    return(
+        <>
+        <h1>{counter}</h1>
+        <input
+        placeholder="Please enter the number"
+        value={stepInput}
+        onChange={(e)=>{
+            setStepInput(e.target.value)
+            const rawValue = Number(e.target.value)
+            if(rawValue > 0){
+                setStep(rawValue)
+            }
+        }}
+        />
+        <button
+        onClick={()=>{
+            setCounter(counter + step)
+        }}
+        >Increase</button>
+        <button
+        onClick={()=>{
+            setCounter(counter - step)
+        }}
+        >Decrease</button>
+        <button onClick={()=>{
+            setCounter(0)
+        }}>Reset</button>
+        </>
+    )
 
-return(
-    <>
-    <h1>{counter}</h1>
-       {console.log(step)}
-    <input 
-    type="number"
-    value={step}
-    placeholder="Enter the number" 
-    
-    onChange={(e)=>{
-        const value = (Number(e.target.value))
-        if(value > 0){
-            setStep(value)
-        }
-    }} />
-    <button onClick={()=>{
-        setCounter(counter+step)
-    }} >+</button>
-    <button onClick={()=>{
-        setCounter(counter-step)
-    }} >-</button>
-    <button onClick={()=>{setCounter(0)}}>Reset</button>
- 
-    </>
-)
 }
 
-export default Counter;
+export default Counter
